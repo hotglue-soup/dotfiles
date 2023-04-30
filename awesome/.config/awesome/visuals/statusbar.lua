8d3f60476f1e3b750d2ce9b3b40c65cda3ff1e7f
Sophie <hotglue.soup@gmail.com>
2023-04-29 20:52:52 -0400

    awesome: fix unclickable tag buttons

--- awesome/.config/awesome/visuals/statusbar.lua
+++ awesome/.config/awesome/visuals/statusbar.lua
@@ -1,67 +1,78 @@
 -- Standard awesome library
 local gears = require("gears")
-local awful = require("awful")
+local awful     = require("awful")
+
+-- Wibox handling library
+local wibox = require("wibox")
+
+-- Custom Local Library: Common Functional visualsration
+local visuals = {
+  wallpaper = require("visuals.wallpaper"),
+  taglist   = require("visuals.taglist"),
+  tasklist  = require("visuals.tasklist")
+}
+
+local taglist_buttons  = visuals.taglist()
+local tasklist_buttons = visuals.tasklist()
 
 local _M = {}
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
 -- Wibar
-
--- Wibox handling library
-local wibox = require("wibox")
 
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-  
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-  
-    -- Create an imagebox widget 
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-      awful.button({ }, 1, function () awful.layout.inc( 1) end),
-      awful.button({ }, 3, function () awful.layout.inc(-1) end),
-      awful.button({ }, 4, function () awful.layout.inc( 1) end),
-      awful.button({ }, 5, function () awful.layout.inc(-1) end)
-    ))
-  
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
-    }
-  
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
-    }
-  
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
-  
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-      layout = wibox.layout.align.horizontal,
-      { -- Left widgets
-        layout = wibox.layout.fixed.horizontal,
-        RC.launcher,
-        s.mytaglist,
-        s.mypromptbox,
-      },
-      s.mytasklist, -- Middle widget
-      { -- Right widgets
-        layout = wibox.layout.fixed.horizontal,
-        mykeyboardlayout,
-        wibox.widget.systray(),
-        mytextclock,
-        s.mylayoutbox,
-      },
-    }
-  end)
+  -- Wallpaper
+  set_wallpaper(s)
+
+  -- Create a promptbox for each screen
+  s.mypromptbox = awful.widget.prompt()
+
+  -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  s.mylayoutbox = awful.widget.layoutbox(s)
+  s.mylayoutbox:buttons(gears.table.join(
+    awful.button({ }, 1, function () awful.layout.inc( 1) end),
+    awful.button({ }, 3, function () awful.layout.inc(-1) end),
+    awful.button({ }, 4, function () awful.layout.inc( 1) end),
+    awful.button({ }, 5, function () awful.layout.inc(-1) end)
+  ))
+
+  -- Create a taglist widget
+  s.mytaglist = awful.widget.taglist {
+    screen  = s,
+    filter  = awful.widget.taglist.filter.all,
+    buttons = taglist_buttons
+  }
+
+  -- Create a tasklist widget
+  s.mytasklist = awful.widget.tasklist {
+    screen  = s,
+    filter  = awful.widget.tasklist.filter.currenttags,
+    buttons = tasklist_buttons
+  }
+
+  -- Create the wibox
+  s.mywibox = awful.wibar({ position = "top", screen = s })
+
+  -- Add widgets to the wibox
+  s.mywibox:setup {
+    layout = wibox.layout.align.horizontal,
+    { -- Left widgets
+      layout = wibox.layout.fixed.horizontal,
+      RC.launcher,
+      s.mytaglist,
+      s.mypromptbox,
+    },
+    s.mytasklist, -- Middle widget
+    { -- Right widgets
+      layout = wibox.layout.fixed.horizontal,
+      mykeyboardlayout,
+      wibox.widget.systray(),
+      mytextclock,
+      s.mylayoutbox,
+    },
+  }
+end)
